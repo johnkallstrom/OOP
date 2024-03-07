3cb3963916a1fb28406f62a1ab3a149cfe875a66
John Källström <johnkaellstrom@gmail.com>
2024-03-07 16:49:38 +0100

    Handler class for creating, updating and deleting/resetting persons

---
+++ Encapsulation/PersonHandler.cs
@@ -0,0 +1,36 @@
+﻿namespace Encapsulation
+{
+	internal class PersonHandler
+	{
+		public void SetAge(Person person, int value)
+		{
+			person.Age = value;
+		}
+
+		public Person CreatePerson(int age, string firstName, string lastName, double height, double weight)
+		{
+			return new Person
+			{
+				Age = age,
+				FirstName = firstName,
+				LastName = lastName,
+				Height = height,
+				Weight = weight
+			};
+		}
+
+		public void UpdatePerson(Person personToUpdate, int age, string firstName, string lastName, double height, double weight)
+		{
+			personToUpdate.Age = age;
+			personToUpdate.FirstName = firstName;
+			personToUpdate.LastName = lastName;
+			personToUpdate.Height = height;
+			personToUpdate.Weight = weight;
+		}
+
+		public void DeletePerson(Person personToDelete)
+		{
+			personToDelete = null;
+		}
+	}
+}
