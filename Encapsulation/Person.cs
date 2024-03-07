cf61c0ac9962b67cb208a1e5efc47ff6bb1a3c74
John Källström <johnkaellstrom@gmail.com>
2024-03-07 15:26:03 +0100

    Add project files.

---
+++ Encapsulation/Person.cs
@@ -0,0 +1,41 @@
+﻿namespace Encapsulation
+{
+	internal class Person
+	{
+		private int age;
+		private string firstName;
+		private string lastName;
+		private int height;
+		private int weight;
+
+        public int Age 
+		{ 
+			get { return age; } 
+			set { age = value; } 
+		}
+
+        public string FirstName 
+		{ 
+			get { return firstName; } 
+			set { firstName = value; } 
+		}
+
+		public string LastName
+		{
+			get { return lastName; }
+			set { lastName = value; }
+		}
+
+		public int Height
+		{
+			get { return height; }
+			set { height = value; }
+		}
+
+		public int Weight
+		{
+			get { return weight; }
+			set { weight = value; }
+		}
+	}
+}
