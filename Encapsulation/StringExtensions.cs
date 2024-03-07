30971a9fed85a5ee19979808eb250e3a7e7f2fce
John Källström <johnkaellstrom@gmail.com>
2024-03-07 16:17:05 +0100

    Check string length

---
+++ Encapsulation/StringExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Encapsulation
+{
+	internal static class StringExtensions
+	{
+		public static bool IsBetween(this string value, int min, int max)
+		{
+			return value.Length >= min && value.Length <= max;
+		}
+	}
+}
