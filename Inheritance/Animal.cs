596034924c725278f626010e7e843afb1d2390da
John Källström <johnkaellstrom@gmail.com>
2024-03-08 14:17:05 +0100

    Animal base class

---
+++ Inheritance/Animal.cs
@@ -0,0 +1,20 @@
+﻿namespace Inheritance
+{
+	internal abstract class Animal
+	{
+		public string Name { get; set; }
+        public int Age { get; set; }
+        public double Weight { get; set; }
+        public string Color { get; set; }
+
+        public abstract void DoSound();
+
+        protected Animal(string name, int age, double weight, string color)
+        {
+            Name = name;
+            Age = age;
+            Weight = weight;
+            Color = color;
+        }
+    }
+}
