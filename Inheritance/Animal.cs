9fb469aef1d1e104f422f296372d39922686bbee
John Källström <johnkaellstrom@gmail.com>
2024-03-08 14:24:50 +0100

    Add mood levels

--- Inheritance/Animal.cs
+++ Inheritance/Animal.cs
@@ -6,15 +6,17 @@
         public int Age { get; set; }
         public double Weight { get; set; }
         public string Color { get; set; }
+        public MoodLevel Mood { get; set; }
 
         public abstract void DoSound();
 
-        protected Animal(string name, int age, double weight, string color)
+        protected Animal(string name, int age, double weight, string color, MoodLevel mood)
         {
             Name = name;
             Age = age;
             Weight = weight;
             Color = color;
+            Mood = mood;
         }
     }
 }
