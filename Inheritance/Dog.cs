d8011ec9a780251ed3b5b82efd6cce4ab42c8f93
John Källström <johnkaellstrom@gmail.com>
2024-03-08 14:17:39 +0100

    Add dog class

---
+++ Inheritance/Dog.cs
@@ -0,0 +1,24 @@
+﻿using Inheritance.Enums;
+
+namespace Inheritance
+{
+	internal class Dog : Animal
+	{
+        public MoodLevel Mood { get; set; }
+
+        public Dog(string name, int age, double weight, string color, MoodLevel mood) : base(name, age, weight, color)
+		{
+			Mood = mood;
+		}
+
+		public override void DoSound() => Console.WriteLine("Bark! Bark! Bark!");
+
+		public void Play() => Console.WriteLine($"{Name} plays!");
+		public void Sleep() => Console.WriteLine($"{Name} sleeps!");
+
+		public override string ToString()
+		{
+			return $"{Name} - {Age} years old, {Weight} kg, {Color} color, {Mood}";
+		}
+	}
+}
---
+++ Inheritance/Enums/MoodLevel.cs
@@ -0,0 +1,10 @@
+﻿namespace Inheritance.Enums
+{
+	internal enum MoodLevel
+	{
+		Happy = 1,
+		Sad = 2,
+		Tired = 3,
+		Excited = 4,
+	}
+}
