7ed71b8784235c27a06cae2fa5c96720fc1711ea
John Källström <johnkaellstrom@gmail.com>
2024-03-08 13:21:53 +0100

    Add char and date error classes

---
+++ Polymrphism/CharInputError.cs
@@ -0,0 +1,10 @@
+﻿namespace Polymrphism
+{
+	internal class CharInputError : UserError
+	{
+		public override string UEMessage()
+		{
+			return "You tried to use a character input in a numeric only field. This fired an error!";
+		}
+	}
+}
---
+++ Polymrphism/DateInputError.cs
@@ -0,0 +1,10 @@
+﻿namespace Polymrphism
+{
+	internal class DateInputError : UserError
+	{
+		public override string UEMessage()
+		{
+			return "You tried to use a date input in a text only field. This fired an error!";
+		}
+	}
+}
