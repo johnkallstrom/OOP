ad30af8bad891a19e533f1f5adb7114cd1318a5f
John Källström <johnkaellstrom@gmail.com>
2024-03-08 14:38:51 +0100

    Display horse actions

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -3,7 +3,16 @@ Console.WriteLine();
 
 var dog = new Dog("Loke", age: 8, weight: 13, "Light brown", MoodLevel.Happy);
 Console.WriteLine(dog.ToString());
+dog.DoSound();
 dog.Play();
 dog.Sleep();
+
+Console.WriteLine();
+
+var horse = new Horse("Waygon", age: 18, weight: 700, "Black", MoodLevel.Excited);
+Console.WriteLine(horse.ToString());
+horse.DoSound();
+horse.Jump();
+horse.Eat("carrots");
 
 Console.ReadKey();
