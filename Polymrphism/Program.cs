9f457a40426df31f1d191be6e34ff14dd92b47b8
John Källström <johnkaellstrom@gmail.com>
2024-03-08 11:50:10 +0100

    Print error messages

--- Polymrphism/Program.cs
+++ Polymrphism/Program.cs
@@ -1,4 +1,21 @@
-﻿Console.WriteLine("3.2 Polymorphism");
+﻿using Polymrphism;
+
+Console.WriteLine("3.2 Polymorphism");
 Console.WriteLine();
+
+var userErrors = new List<UserError>
+{
+	new NumericcInputError(),
+	new NumericcInputError(),
+	new NumericcInputError(),
+	new TextInputError(),
+	new TextInputError(),
+	new TextInputError(),
+};
+
+foreach (var error in userErrors)
+{
+    Console.WriteLine(error.UEMessage());
+}
 
 Console.ReadKey();
