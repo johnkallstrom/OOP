0d0546511b9f36990d37279b5b689c00a33bd503
John Källström <johnkaellstrom@gmail.com>
2024-03-12 11:17:33 +0100

    Remove comments

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -1,27 +1,6 @@
 ﻿Console.WriteLine("3.3 Inheritance");
 Console.WriteLine();
 
-// F: Om vi under utvecklingen kommer fram till att samtliga fåglar behöver ett nytt attribut, i vilken klass bör vi lägga det?
-// S: Bird
-
-// F: Om alla djur behöver det nya attributet, vart skulle man lägga det då?
-// S: Animal
-
-// F: Försök att lägga till en häst i listan av hundar. Varför fungerar inte det?
-// S: Det fungerar inte därför att en häst och en hund är inte samma typ. Häst och hund är två olika subklasser av basklassen Animal
-
-// F: Vilken typ måste listan vara för att alla klasser skall kunna lagras tillsammans?
-// S: Animal
-
-// F: Skriv ut samtliga Animals Stats() genom en foreach loop. Förklara vad det är som händer
-// S: Listan av animals innehåller olika klasser som alla ärver av basklassen Animal. Foreach-loopen går igenom hela listan och skriver ut statistik för varje djur. 
-// Eftersom metoden Stats() finns deklarerad i basklassen Animal så kommer varje subklass åt den (dvs alla djur som ärver av Animal) och vi kan på de sättet skriva ut statistiken för varje djur. 
-// I och med att metoden är virtual kan vi också skriva över den i de subklasser där vi vill kanske lägga till lite extra information, specifik för just den klassen/djuret.
-
-// F: Skapa en ny metod med valfritt namn i klassen Dog som endast returnerar en valfri sträng. Kommer du åt den metoden från Animals listan?
-// S: Nej, eftersom variabeln "animal" i foreach-loopen är av typen Animal och inte Dog så har vi inte tillgång till den metoden.
-// Bara de fält, egenskaper och metoder som ligger i Animal klassen kommer vi åt.
-
 var animals = new List<Animal>
 {
 	new Dog("Sally", 9, 24, "Gray", Mood.Happy, Gender.Female, isMixedBreed: true),
